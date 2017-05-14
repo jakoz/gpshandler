cdc82dd0248e749e5cd8b330dc8dde0c29c28cb3
Jakub Kozlowski <jakub.kozlowski@gmx.com>
2017-05-14 22:21:16 +0200

    httpServ

--- server.js
+++ server.js
@@ -5,16 +5,32 @@ var bodyParser = require('body-parser');
 
 //connect to mongoDB:
 mongoose.connect("mongodb://kuba:test123@ds155820.mlab.com:55820/gpsdb");
+var http = require('http');
+var qs = require('querystring');
 
+http.createServer(function(request, response) {
+    console.log(request.url);
+    console.log(request.method);
+    var body = '';
+    request.on('data', function(data) {
+        body += data;
+    });
+    request.on('end', function(data) {
+        console.log(qs.parse(body));
+    });
+
+    response.end('');
+
+    var app = express();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    //routes:
+    app.use('/', require('./routes/api'));
+}).listen(6969, "0.0.0.0");
 //express:
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-//routes:
-app.use('/', require('./routes/api'));
-
-//start server:
-app.listen(6969, "0.0.0.0");
+// //start server:
+// app.listen(6969, "0.0.0.0");
 
 console.log('Server is running on port 6969')
